b4c7e525a32d71bbb34ce0580c09a4c1a4eeb882
Simon Jones <simonjonessays01@gmail.com>
2021-11-04 13:35:07 +0000

    added full movement

--- Dungeon.java
+++ Dungeon.java
@@ -2,8 +2,8 @@ public class Dungeon{
                 String input = "w";
 
 
-        int charPosition = 609;
-        int oldCharPosition = 609;
+        int charPosition = 607;
+        int oldCharPosition = 607;
         String playerMarker = "@";
 
         //Map Key: 
@@ -13,46 +13,77 @@ public class Dungeon{
         //# = Enemy
         //& = Enemy
         private String map =
-                "----------------------------------------------------------------------------------------------------\n"+
-                "|                                                                                                  |\n"+
-                "|                                                                                             ?    |\n"+
-                "|                                              !                                                   |\n"+
-                "|                                                                                                  |\n"+
-                "|                                                                                                  |\n"+
-                "|      @                                                                                           |\n"+
-                "|                                                                                                  |\n"+
-                "|                                                             !                                    |\n"+
-                "|                                                                                                  |\n"+
-                "|                                                                                                  |\n"+
-                "|                                                                                                  |\n"+
-                "------------------------------------------------------------------------------         -------------\n"+
-                "-----------------------------------------------------------------------------|         |------------\n"+
-                "|                                               |----------------------------|    #    |------------\n"+
-                "|                                          ?    |----------------------------|         |------------\n"+
-                "|                                               |-----------------------------         -------------\n"+
-                "|                      ---------------          |--------|                                         |\n"+
-                "|                      |-------------|          |--------|                                    ?    |\n"+
-                "|                      |-------------|          |--------|                                         |\n"+
-                "|          &           |-------------|          |--------|                                         |\n"+
-                "|                      |-------------|          ----------       #                                 |\n"+
-                "|                      |-------------|                                                             |\n"+
-                "|                      |-------------|     #                                             #         |\n"+
-                "|                      |-------------|                                                             |\n"+
-                "|                      --------------|          ----------                                         |\n"+
-                "|                                |---|          |--------|                                         |\n"+
-                "|                                |---|          |--------|                                         |\n"+
-                "|                                |---|?         |--------|                                         |\n"+
-                "|---------------------------------------------------------------------------------------------------";
+                "---------------------------------------------------------------------------------------------------\n"+
+                "|                                                                                                 |\n"+
+                "|                                                                                            ?    |\n"+
+                "|                                            !                                                    |\n"+
+                "|                                                                                                 |\n"+
+                "|                                                                                                 |\n"+
+                "|      @                                                                                          |\n"+
+                "|                                                                                                 |\n"+
+                "|                                                           !                                     |\n"+
+                "|                                                                                                 |\n"+
+                "|                                                                                                 |\n"+
+                "|                                                                                                 |\n"+
+                "----------------------------------------------------------------------------          -------------\n"+
+                "---------------------------------------------------------------------------|          |------------\n"+
+                "|                                               |--------------------------|    #     |------------\n"+
+                "|                                          ?    |--------------------------|          |------------\n"+
+                "|                                               |---------------------------          -------------\n"+
+                "|                      ---------------          |--------|                                        |\n"+
+                "|                      |-------------|          |--------|                                   ?    |\n"+
+                "|                      |-------------|          |--------|                                        |\n"+
+                "|          &           |-------------|          |--------|                                        |\n"+
+                "|                      |-------------|          ----------       #                                |\n"+
+                "|                      |-------------|                                                            |\n"+
+                "|                      |-------------|     #                                           #          |\n"+
+                "|                      |-------------|                                                            |\n"+
+                "|                      --------------|          ----------                                        |\n"+
+                "|                                |---|          |--------|                                        |\n"+
+                "|                                |---|          |--------|                                        |\n"+
+                "|                                |---|?         |--------|                                        |\n"+
+                "---------------------------------------------------------------------------------------------------";
         public void Print(){
                 System.out.println(map);
         }
         public void  Move(String input){
                 if(input == "w"){
                         charPosition -= 100;
-                        if((map.substring(0, charPosition) == "-") || (map.substring(0, charPosition) == "|")){
+                        if(map.substring(0, charPosition) == "-"){
                                 charPosition += 100;
+				
                         }else{
-                                map = map.substring(0, charPosition) + playerMarker + map.substring(charPosition + 1);
+                                map = map.substring(0, charPosition) + playerMarker + map.substring(charPosition +1);
+                                map = map.substring(0, oldCharPosition) + " " + map.substring(oldCharPosition + 1);
+                                oldCharPosition = charPosition;
+				}
+		}if(input == "a"){
+                        charPosition -= 1;
+                        if(map.substring(0, charPosition) == "|"){
+                                charPosition += 1;
+				
+                       	}else{
+                                map = map.substring(0, charPosition) + playerMarker + map.substring(charPosition +1);
+                                map = map.substring(0, oldCharPosition) + " " + map.substring(oldCharPosition + 1);
+                                oldCharPosition = charPosition;
+				}
+		}if(input == "a"){
+                        charPosition += 100;
+                        if(map.substring(0, charPosition) == "-"){
+                                charPosition -= 100;
+	
+                        }else{
+                                map = map.substring(0, charPosition) + playerMarker + map.substring(charPosition +1);
+                                map = map.substring(0, oldCharPosition) + " " + map.substring(oldCharPosition + 1);
+                                oldCharPosition = charPosition;
+			}
+		}if(input == "d"){
+                        charPosition += 1;
+                        if(map.substring(0, charPosition) == "|"){
+                                charPosition -= 1;
+				
+                        }else{
+                                map = map.substring(0, charPosition) + playerMarker + map.substring(charPosition +1);
                                 map = map.substring(0, oldCharPosition) + " " + map.substring(oldCharPosition + 1);
                                 oldCharPosition = charPosition;
 
