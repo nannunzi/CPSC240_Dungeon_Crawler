4491990db992f26c59bb1ca5672959bf45c4c2bf
Nicholas Annunziata <nannunzi@mail.umw.edu>
2021-11-04 06:45:33 +0000

    one last thing

--- Main.java
+++ Main.java
@@ -8,20 +8,15 @@ public class Main{
         while(choos!=6){
                 System.out.println("What would you like to do?");
                 System.out.println("-----------------------------");
-                System.out.println("1. Print inventory \n2. Add Random Item\n3. Drop item \n4. Equip Weapon \n5. Equip Armor\n6. Exit");
+                System.out.println("1. Print inventory \n2. move\n3. Drop item \n4. Equip Weapon \n5. Equip Armor\n6. Exit");
                 choos = in.nextInt();
                 switch (choos) {
                         case 1: inventory.print();
                                 break;
-                        case 2: if(inventory.add(theGen.generate()))
-                                {
-                                 System.out.println("added random item");
-                                break;
-                                }
-                                else{
-                                       System.out.println("cannot add item, inventory too full.");
-                                        break;
-                                }
+			case 2: while(!(choos.equalsIgnoreCase("q"))){
+
+			}
+                         	break;       
                         case 3: inventory.drop();
                                 break;
                         case 4: inventory.equipWeapon();
@@ -51,8 +46,9 @@ public class Main{
                 }
         }
 }
-~           
-
+           
+/*
+ * code scrapyard
 	public static void main(String[] args){
 		Dungeon dungeon = new Dungeon();
 		String input = "w";
@@ -70,4 +66,14 @@ public class Main{
 		dungeon.Move(input);
                 dungeon.Print();
 	}
-}
+	if(inventory.add(theGen.generate()))
+                                {
+                                 System.out.println("added random item");
+                                break;
+                                }
+                                else{
+                                       System.out.println("cannot add item, inventory too full.");
+                                        break;
+                                }
+				*/
+
